db6a9dc74fa9b18e0c0c93881a99f6fe905d07e6
Maria Sanchez <clarasanchez2398@gmail.com>
2021-07-22 20:41:52 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,21 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome : String = "Maria"
-    var cpf : String = "128.348.496-09"
-    private set
+abstract class Pessoa (
+   val name:String,
+   val cpf : String
 
-
-    constructor()
-
-    fun person() = "$nome e $cpf"
-
-
-}
-
-fun main() {
-    val maria = Pessoa()
-
-    println(maria.person())
-
-}
+)
